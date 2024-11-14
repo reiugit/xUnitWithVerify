e16c5acdc783bdde3183fe92a21450d36fe0ba06
reiugit <67372053+reiugit@users.noreply.github.com>
2024-11-14 07:52:31 +0100

    Integrated an error for testing the github action.

--- xUnitWithVerify/ClassBeingTested.cs
+++ xUnitWithVerify/ClassBeingTested.cs
@@ -5,5 +5,5 @@ public record Person(string FirstName, string LastName, int Age);
 internal static class ClassBeingTested
 {
     public static Person GetJohnDoe()
-        => new("John", "Doe", 30); // This is the demo-method we want to test
+        => new("John", "Doe", 36); // This is the demo-method we want to test
 }
